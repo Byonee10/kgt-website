4ab40fe0cfe8a76b9e640980d906ff0412d952e2
Ramazan Çağlar Bulgay <65749103+Byonee10@users.noreply.github.com>
2025-06-15 21:36:48 +0300

    Update navtabs.tsx

--- src/components/navtabs.tsx
+++ src/components/navtabs.tsx
@@ -9,7 +9,7 @@ function samePageLinkNavigation(
 ) {
   if (
     event.defaultPrevented ||
-    event.button !== 0 || // ignore everything but left-click
+    event.button !== 0 || 
     event.metaKey ||
     event.ctrlKey ||
     event.altKey ||
